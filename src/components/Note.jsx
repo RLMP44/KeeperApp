4b132a22b3f43e9f47cdc796a188551a4c84f831
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-10-24 16:02:35 +0000

    add: note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>First note</h1>
+      <p>Note info is here</p>
+    </div>
+  );
+}
+
+export default Note;
