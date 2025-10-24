635a5ea2b23447cb54f767c044c8a90dd73d5d0a
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-10-24 15:52:01 +0000

    add: set up react and App component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 //1. Create a new React app.
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./components/App";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<App />);
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
